9b045f0a9fd85164b9732023b233f8aedaa910f1
Roshan-Ojha <roshanojha65@gmail.com>
2023-12-03 00:25:38 +0545

    developed front page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Poppins } from 'next/font/google'
+
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] ,variable: '--font-inter',display: 'swap',})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  display: 'swap',
+  weight: ['100','200','300','400','500','600', '700'],
+  
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" >
+      <body className={`${inter.variable} ${poppins.variable}`} suppressHydrationWarning={true}>{children}</body>
     </html>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+    LOG_LEVEL: "ERROR"
+  }
+}
 
 module.exports = nextConfig
