264d65247fc64069c3c854841e2d16327fd87b02
Roshan-Ojha <roshanojha65@gmail.com>
2023-12-05 21:12:54 +0545

    Frontend completed I guess

--- app/register/Verify.tsx
+++ app/register/Verify.tsx
@@ -1,15 +1,66 @@
+import { use, useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Verify() {
+interface Email {
+  useremail: string;
+}
+
+export default function Verify(props: Email) {
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter()
+
+  async function VerifyOTP() {
+    try {
+
+     
+      const response = await fetch("http://127.0.0.1:4000/verify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: props.useremail,
+          otp: otp,
+        })
+      });
+
+      if (!response.ok) {
+        console.log("Hello")
+        const errorData = await response.json();
+        setError(errorData.error);
+      } else {
+        const data = await response.json();
+        router.push("/")
+
+      }
+    } catch (error) {
+      console.log("her?")
+      setError("Some Error Occured");
+    }
+  }
+
   return (
     <div className="flex justify-center items-center">
-      <div className="h-[39px] w-[526px] flex self-center mt-[100px]  justify-between items-center rounded-[20px] shadow-[0_5px_5px_-3px_rgba(0,0,0,0.2)]">
+      <div>{props.useremail}</div>
+      <div className="relative h-[39px] w-[526px] flex self-center mt-[100px]  justify-between items-center rounded-[20px] shadow-[0_5px_5px_-3px_rgba(0,0,0,0.2)]">
         <input
-          type= "text"
+          type="text"
           placeholder="Enter your OTP"
           maxLength={6}
           className="ml-[10px] h-[39px] w-[390px] bg-transparent focus:outline-none"
+          onChange={(e) => setOtp(e.target.value)}
         ></input>
-        <button className="flex  justify-center items-center h-[39px] w-[121px] bg-[#4e6cdb] font-poppins rounded-[23px] text-[12px] text-[#cfe0e4] hover:bg-[#cfe0e4] hover:text-[#4e6cdb] hover:border-[1px] hover:border-[#4e6cdb]  max-[393px]:h-[31px] max-[393px]:w-[71px]  max-[393px]:text-[12px]">
+        {error && (
+          <div className="absolute text-red-700 w-full top-[50px] flex justify-center">
+            {error}
+          </div>
+        )}
+        <button
+          className="flex  justify-center items-center h-[39px] w-[121px] bg-[#4e6cdb] font-poppins rounded-[23px] text-[12px] text-[#cfe0e4] hover:bg-[#cfe0e4] hover:text-[#4e6cdb] hover:border-[1px] hover:border-[#4e6cdb]  max-[393px]:h-[31px] max-[393px]:w-[71px]  max-[393px]:text-[12px]"
+          onClick={VerifyOTP}
+        >
           Verify
         </button>
       </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,15 +2,50 @@
 
 import React, { useState } from "react";
 import Verify from "./Verify";
-import { useRouter } from "next/navigation";
+
 
 export default function Register() {
   const [registered, setregistered] = useState(false);
   const [email, setEmail] = useState("");
+  const [error, seterror] = useState("");
 
-  function RegisterEmail() {
-    setregistered(true);
+  async function RegisterEmail() {
+    if (!(email.includes("@") || email.includes("."))) {
+      seterror("Invalid Email");
+    } else {
 
+      try {
+        const response = await fetch("http://127.0.0.1:4000/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: email
+          })
+        });
+      
+        if (!response.ok) {
+          // Handle any error status code generically
+          // We can parse the error response if needed
+          const errorData = await response.json();
+          console.error("Error:", errorData);
+          // For instance, you can set an error state or display an error message
+          seterror(errorData.error);
+        } else {
+          const data = await response.json();
+          console.log(data);
+          setregistered(true)
+          // Process the data or set any state variables here
+        }
+      } catch (error) {
+        // Handle network errors or other exceptions
+        console.error("Network error:", error);
+        // For instance, set an error state or display a generic error message
+        seterror("Unknown Error");
+      }
+      
+    }
   }
 
   return (
@@ -19,25 +54,36 @@ export default function Register() {
         <span>Quote</span>
         <br></br>
         <span>Oasis</span>
-      </div>
+      </div> 
 
       <div className="flex self-center text-[24px] text-[#030303] font-poppins">
         Get Inspired, Subscribet to Quote Oasis
       </div>
 
-      {registered?<Verify/>:<div className="h-[39px] w-[526px] flex self-center mt-[100px]  justify-between items-center rounded-[20px] shadow-[0_5px_5px_-3px_rgba(0,0,0,0.2)]">
-        <input
-          type="text"
-          placeholder="Enter your Email Here"
-          className="ml-[10px] h-[39px] w-[390px] bg-transparent focus:outline-none"
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <button className="flex  justify-center items-center h-[39px] w-[121px] bg-[#4e6cdb] font-poppins rounded-[23px] text-[12px] text-[#cfe0e4] hover:bg-[#cfe0e4] hover:text-[#4e6cdb] hover:border-[1px] hover:border-[#4e6cdb]  max-[393px]:h-[31px] max-[393px]:w-[71px]  max-[393px]:text-[12px]"
-        onClick={RegisterEmail}
-        >
-          Register
-        </button>
-      </div>}
+      {registered ? (
+        <Verify useremail={email} />
+      ) : (
+        <div className="relative h-[39px] w-[526px] flex self-center mt-[100px]  justify-between items-center rounded-[20px] shadow-[0_5px_5px_-3px_rgba(0,0,0,0.2)]">
+          <input
+            required
+            type="text"
+            placeholder="Enter your Email Here"
+            className="ml-[10px] h-[39px] w-[390px] bg-transparent focus:outline-none"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          {error && (
+            <div className="absolute text-red-700 w-full top-[50px] flex justify-center">
+              {error}
+            </div>
+          )}
+          <button
+            className="flex  justify-center items-center h-[39px] w-[121px] bg-[#4e6cdb] font-poppins rounded-[23px] text-[12px] text-[#cfe0e4] hover:bg-[#cfe0e4] hover:text-[#4e6cdb] hover:border-[1px] hover:border-[#4e6cdb]  max-[393px]:h-[31px] max-[393px]:w-[71px]  max-[393px]:text-[12px]"
+            onClick={RegisterEmail}
+          >
+            Register
+          </button>
+        </div>
+      )}
     </div>
   );
 }
